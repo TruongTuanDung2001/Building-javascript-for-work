5beaee465eba6eec69a437a2192c15594663fa87
truongtuandung2001 <truongtuandung2001@gmail.com>
2026-05-26 22:44:14 +0700

    Dom event bubbling / stopPropagation

--- DOM/event.js
+++ DOM/event.js
@@ -10,12 +10,23 @@ inputEvent.addEventListener('input', function(e){ // e là biến phát sinh ra 
     console.log(e.target.value);
 });
 
-//Submit
+// Submit
 let formSubmit = document.getElementById('form-submit');
 let inputSubmit = document.querySelector('.input-submit');
 let btnSubmit = document.getElementById('btn-submit');
 
 formSubmit.addEventListener('submit', function(e){
-    e.preventDefault();
+    e.preventDefault(); // ngăn chặn việc form chuyển dữ liệu và load lại trang nha
     console.log('Submited !!!');
 });
+
+// Bubbling / stopPropagation
+let parent = document.getElementById('parent');
+let child = document.getElementById('child');
+
+child.addEventListener('click', (e) => {
+    e.stopPropagation(); // ngăn chặn sủi bọt bubbling lan ra thẻ cha
+    console.log('Click child');
+});
+parent.addEventListener('click', () => console.log('Click parent'));
+//Bubbling là sủi bọt, nó sử lý sử kiện ở con và lên tiếp sự kiện ở thẻ cha
