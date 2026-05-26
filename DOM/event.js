a42579c670adfc6fa7f57040193a5bcd1bdcfeb5
truongtuandung2001 <truongtuandung2001@gmail.com>
2026-05-26 23:00:41 +0700

    Dom event delegation, e.target

--- DOM/event.js
+++ DOM/event.js
@@ -30,3 +30,16 @@ child.addEventListener('click', (e) => {
 });
 parent.addEventListener('click', () => console.log('Click parent'));
 //Bubbling là sủi bọt, nó sử lý sử kiện ở con và lên tiếp sự kiện ở thẻ cha
+
+// Delegation
+// Cách 1 tốn nhiều addEventListener
+let item = document.querySelectorAll('.item');
+// item.forEach((i) => {
+//     i.addEventListener('click', () => console.log(i.innerText));
+// })
+
+// Cách 2: Delegation: thẻ cha quản lý nhiều thẻ con
+let list = document.querySelector('.list');
+list.addEventListener('click', function(e){ //e là các thẻ li con trong thẻ ul cha, nên gán sự kiện cho thẻ cha thì khi e.target click tức là thẻ con li được click. Thẻ nào click thì show nội dung thẻ đó ra, không cần thêm mối thẻ con li 1 addEventListener làm gì
+    console.log(e.target.textContent);
+});
