c610bcfb4d955ecece89eed9c8983ad10746e39a
truongtuandung2001 <truongtuandung2001@gmail.com>
2026-05-27 10:21:42 +0700

    Set timeout, set interval, important debounce search (clearTimeout)

---
+++ DOM/browser.js
@@ -0,0 +1,54 @@
+// BROWSER API JS
+// settimeOut
+let timeOut = setTimeout(function(){
+    console.log('I Love You');
+}, 2000);
+//Sau 2 giây -> chạy, chỉ chạy 1 lần duy nhất
+
+//setInterval
+let interval = setInterval(() => {
+    console.log('18H sân Đất Việt');
+}, 3000);
+//Sau 10 giây -> chạy, chạy liên tục
+
+//ngắt 
+clearTimeout(timeOut); //ngắt setTimeout
+clearInterval(interval); //ngắt interval
+
+// Countdown 5 4 3 2 1 go
+let numberCount = 6;
+let countDown = setInterval(function(){
+    if(numberCount <= 1){
+        numberCount = 'Go';
+        clearInterval(countDown);
+    }else{
+        numberCount--;
+    }
+    console.log(numberCount);
+}, 1000);
+clearInterval(countDown);
+//change image
+//https://i.pinimg.com/736x/e1/eb/e3/e1ebe369e6cf740cb924d566893ad264.jpg
+let imageBox = document.getElementById('image-box');
+let num = 0;
+setInterval(()=>{
+    if(num == 0){
+        imageBox.src = 'https://i.pinimg.com/736x/e1/eb/e3/e1ebe369e6cf740cb924d566893ad264.jpg';
+        num++;
+    }
+    else{
+        imageBox.src = 'https://i.pinimg.com/736x/06/54/72/0654722f51fee0a18ca11c3d497ae6dd.jpg';
+        num--;
+    }
+}, 2000);
+
+//Debounce search
+let time;
+let debounceSearch = document.querySelector('.debounceSearch');
+debounceSearch.addEventListener('input', function(e){
+    clearTimeout(time); //nếu kh clear: người dùng nhập nhanh 3 lần số 1 thì log ra số 111 (3 lần)
+    //nếu có clear thì, log ra số 111 (1 lần) vì nó clear, chỉ chạy khi người dùng xong
+    time = setTimeout(()=>{
+        console.log(e.target.value);
+    },500)
+});
